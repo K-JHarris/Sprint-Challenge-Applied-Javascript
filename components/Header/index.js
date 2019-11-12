a727ecfb788299dfbf325a9a5da596a784038944
Khari <kharris2k16@gmail.com>
2019-11-12 13:10:28 -0800

    Header component complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,32 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+//declare all component elements inside header function
+function Header() {
+  const headerPackage = document.createElement('div');
+  const dateSpan = document.createElement('span');
+  const headerTitle = document.createElement('h1');
+  const tempSpan = document.createElement('span');
+  //set text content for elements that need it
+  dateSpan.textContent = 'SMARCH 28, 2019';
+  headerTitle.textContent = 'Lambda Times'; 
+  tempSpan.textContent = '98°'
+  //use .appendChild to insert newly created elements into container div
+  headerPackage.appendChild(dateSpan);
+  headerPackage.appendChild(headerTitle);
+  headerPackage.appendChild(tempSpan);
+  //set classes for elements that need them
+  headerPackage.classList.add('header');
+  dateSpan.classList.add('date');
+  tempSpan.classList.add('temp');
+  //return container div, now full of our elements
+  return headerPackage;
+}
+
+//create a reference to the .header-container class
+const headerContainer = document.querySelector('.header-container');
+//append our headerPackage into that reference
+headerContainer.appendChild(Header());
+
+
+// MVP 👌
